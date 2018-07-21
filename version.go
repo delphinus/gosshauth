0ba985d3a5ce5fd03341037f0f79c7adec579ea3
delphinus <delphinus@remora.cx>
2018-07-21 16:10:32 +0900

    Move logic to print version

--- version.go
+++ version.go
@@ -1,5 +1,18 @@
 package gosshauth
 
+import (
+	"fmt"
+
+	"gopkg.in/urfave/cli.v2"
+)
+
+func init() {
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Fprintf(c.App.Writer, "%v version v%v (%s)\n",
+			c.App.Name, c.App.Version, GitCommit)
+	}
+}
+
 // Version is needed for selfupdate.
 const Version = "0.0.4"
 
