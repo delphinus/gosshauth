449a04e00bcdc47d1eaff193d208806bcee6c415
delphinus <delphinus@remora.cx>
2018-07-26 13:17:53 +0900

    Bump version up to v0.0.8

--- version.go
+++ version.go
@@ -26,7 +26,7 @@ func init() {
 }
 
 // Version is needed for selfupdate.
-const Version = "0.0.7"
+const Version = "0.0.8"
 
 // GitCommit is needed for selfupdate.  This will be set in Makefile task.
 var GitCommit = ""
