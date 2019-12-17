7996b18da356837cc466639b59c592877417ff62
delphinus <me@delphinus.dev>
2019-12-17 11:01:25 +0900

    Fix syntax in fish scripts

--- shell_fish.go
+++ shell_fish.go
@@ -9,9 +9,9 @@ func (fish) Export(p string) string {
 	return "set -x SSH_AUTH_SOCK " + p
 }
 func (fish) Hook() string {
-	return `function _sshauthsock_hook --on-event fish_prompt {
+	return `function _sshauthsock_hook --on-event fish_prompt
   set -l previous_exit_status $status
   eval ("` + Me() + `" fixup fish)
   return $previous_exit_status
-}`
+end`
 }
